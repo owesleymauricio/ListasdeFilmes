e3679347c6feaeadd5376c2b5837af5100079899
owesleymauricio <wesleymauricio85@gmail.com>
2023-10-19 19:52:30 -0300

    arrumei

--- movie_list/src/main.jsx
+++ movie_list/src/main.jsx
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route element={<App />}> {/*o primeiro element, e como se fosse definido o elemento pai*/}
-          <Route path='/' element={<Home />} />
+          <Route path='/ListaDeFilmes' element={<Home />} />
           <Route path='movie/:id' element={<Movie />} />
           <Route path='/search' element={<Search />} />
         </Route>
